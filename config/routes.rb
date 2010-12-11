619c0ec745e730e6865fa8d884ffe1871e8f7dcc
Andy <andy@m6300.(none)>
2010-12-10 17:01:36 -0800

    Added intergration tests.
    Added routes to / /about /contact, etc.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 SampleApp::Application.routes.draw do
+
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
+  match '/help', :to => 'pages#help'
+  root :to => "pages#home"
+
   get "pages/home"
   get "pages/contact"
   get "pages/about"
@@ -51,9 +57,6 @@ SampleApp::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
 
   # See how all your routes lay out with "rake routes"
 
