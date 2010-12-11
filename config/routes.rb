e83d8a5d50bed355ec43a13f801a3738a6819f0b
Andy <andy@m6300.(none)>
2010-12-11 10:11:51 -0800

    Finished layout and routes.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ SampleApp::Application.routes.draw do
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
+  match '/signup', :to => 'users#new'
   root :to => "pages#home"
 
   get "pages/home"
---
+++ spec/requests/layout_links_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe "LayoutLinks" do
+
+  it "should have a Home page at '/'" do
+    get '/'
+    response.should have_selector('title', :content => "Home")
+  end
+
+  it "should have a Home page at '/contact'" do
+    get '/contact'
+    response.should have_selector('title', :content => "Contact")
+  end
+
+  it "should have a Home page at '/about'" do
+    get '/about'
+    response.should have_selector('title', :content => "About")
+  end
+
+  it "should have a Home page at '/help'" do
+    get '/help'
+    response.should have_selector('title', :content => "Help")
+  end
+
+  it "should have a Sign up page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign up")
+  end
+
+end
